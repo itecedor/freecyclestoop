a6708f47b99eb22c55ddee395b6d588812a6e231
Ivete Tecedor <yivich@gmail.com>
2012-11-18 09:43:56 -0700

    updated dbconfig with my server info

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,17 @@
 <?php
+// IVETE'S SERVER
+	$link = mysql_connect('localhost', 'knitspir_curbcyc', 'OH@I!PickUpFreeShitHere!');
+  if (!$link) {
+    die('Could not connect: ' . mysql_error());
+  }
+  mysql_select_db('knitspir_curbcycle');
+
+/* CAROLINE'S SERVER
+
+
 	$link = mysql_connect('angelhack.keimdesign.com', 'freecyclestoop', 'OH@I!PickUpFreeShitHere!');
   if (!$link) {
     die('Could not connect: ' . mysql_error());
   }
   mysql_select_db('stoopme');
-?>
+*/
